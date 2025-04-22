11cf7419d3249398a568b317b7020700b76fc8d0
ALEYI <aleelaleyi@gmail.com>
2025-04-22 17:18:37 -0500

    Create a wrapper for the grpc client

--- internal/grpc/grpc_client.go
+++ internal/grpc/grpc_client.go
@@ -1,4 +1,46 @@
 package grpc
 
+import (
+	"context"
+	"ebpf_loader/internal/grpc/pb"
+	"time"
 
+	"google.golang.org/grpc"
+)
+
+type Client struct{
+  conn *grpc.ClientConn
+  client pb.EventCollectorClient
+}
+
+func NewClient(address string) (*Client, error){
+
+  conn, err := grpc.NewClient(address)
+  if err != nil {
+    return nil,err
+  }
+
+  defer conn.Close()
+
+  client := pb.NewEventCollectorClient(conn)
+
+  return &Client{conn: conn,client: client},nil
+}
+
+func (c *Client) Close() error{
+  return c.conn.Close()
+}
+
+func (c *Client) SendEventMessage(ctx context.Context, batch *pb.EbpfEventBatch) (*pb.CollectorAck,error){
+
+  ctx, cancel := context.WithTimeout(ctx, time.Second*5)
+  defer cancel()
+
+  resp, err := c.client.SendEvents(ctx, batch)
+  if err != nil {
+    return nil,err
+  }
+
+  return resp,nil
+}
 
