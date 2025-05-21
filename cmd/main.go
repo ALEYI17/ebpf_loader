522b5cfc81aba029c6a9b58fa270f18f9c837552
ALEYI <aleelaleyi@gmail.com>
2025-05-21 16:47:30 -0500

    fix main to be more clear

--- cmd/main.go
+++ cmd/main.go
@@ -42,6 +42,20 @@ func main() {
 
 	conf := config.LoadConfig()
 
+	var loaders []programs.Load_tracer
+	for _, program := range conf.EnableProbes {
+    loaderInstance , err := loader.NewEbpfLoader(program)
+    if err != nil {
+      logger.Warn("Unsupported or unknown program", zap.String("program", program), zap.Error(err))
+      continue
+    }
+
+    defer loaderInstance.Close()
+    loaders = append(loaders, loaderInstance)
+  }
+  
+  logger.Info("Loader(s) created successfully")
+
   client, err := grpc.NewClient(conf.ServerAdress,conf.Serverport)
 	if err != nil {
     logger.Fatal("Error creating the client", zap.Error(err))
@@ -49,38 +63,6 @@ func main() {
 
   logger.Info(" gRPC Client created successfully")
 
-	var loaders []programs.Load_tracer
-	for _, program := range conf.EnableProbes {
-		switch program {
-		case programs.Loaderexecve:
-			el, err := loader.NewEbpfLoader(program)
-			if err != nil {
-        logger.Fatal("Error creating the execve loader", zap.Error(err))
-			}
-			defer el.Close()
-
-			loaders = append(loaders, el)
-
-		case programs.LoaderOpen:
-			ol, err := loader.NewEbpfLoader(program)
-			if err != nil {
-        logger.Fatal("Error creating the open loader", zap.Error(err))
-			}
-			defer ol.Close()
-			loaders = append(loaders, ol)
-    case programs.LoaderChmod:
-      cl , err := loader.NewEbpfLoader(program)
-      if err != nil{
-        logger.Fatal("Error creating the chmod loader", zap.Error(err))
-      }
-      defer cl.Close()
-      loaders = append(loaders, cl)
-		default:
-      logger.Warn("Unknown program", zap.String("program", program))
-		}
-	}
-  
-  logger.Info("Loader(s) created successfully")
   defer client.Close()
 	if err := client.Run(ctx, loaders, conf.Nodename); err != nil {
     logger.Error("Error running client", zap.Error(err))
