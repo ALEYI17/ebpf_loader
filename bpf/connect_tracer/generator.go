6a86f1892d80b8da2023b6bfdf5a9b2cd8cb9870
ALEYI <aleelaleyi@gmail.com>
2025-05-28 18:33:30 -0500

    creating the loader in progres ...

--- bpf/connect_tracer/generator.go
+++ bpf/connect_tracer/generator.go
@@ -1,3 +1,8 @@
 package connecttracer
 
+import "ebpf_loader/internal/grpc/pb"
+
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 -type socket_event_t   Connecttracer connect_tracer.bpf.c
+
+
+func GenerateGrpcMessage(raw ConnecttracerSocketEventT, nodeName string) *pb.EbpfEvent
